c193716666c32b32bd047838c0c7dafcb44c0654
githubmoore <alexander.moore@inacapmail.cl>
2025-01-15 16:46:22 -0300

    1.1

--- src/main/java/com/githubmoore/challenge_foro_hub/config/SecurityConfig.java
+++ src/main/java/com/githubmoore/challenge_foro_hub/config/SecurityConfig.java
@@ -3,25 +3,24 @@ package com.githubmoore.challenge_foro_hub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/api/auth/**").permitAll()
+            .requestMatchers("/api/auth/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin().disable()
             .httpBasic();
+        return http.build();
     }
 
     @Bean
